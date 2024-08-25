cf798557e0efe1b5ab750bdd1be88d10b558a698
LoordhuJeyakumar <loordhujeyakumar@gmail.com>
2024-08-25 09:41:38 +0530

    delete profile controller methode done

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -67,6 +67,28 @@ const profileController = {
       return res.status(500).json({ error: "Internal server error" });
     }
   },
+  deleteProfile: async (req, res) => {
+    try {
+      const id = req.userId;
+
+      const profile = await ProfileModel.findOne({
+        where: { userId: id },
+      });
+
+      if (!profile) {
+        return res.status(404).json({ error: "Profile not found" });
+      }
+
+      await profile.destroy();
+
+      return res.status(200).json({
+        message: "Profile deleted successfully",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  },
 };
 
 module.exports = profileController;
--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -4,5 +4,15 @@ const { authMiddleware } = require("../middlewares/authMiddleware");
 const profileRouter = require("express").Router();
 
 profileRouter.get("/getProfile", authMiddleware, profileController.getProfile);
+profileRouter.put(
+  "/updateProfile",
+  authMiddleware,
+  profileController.updateProfile
+);
+profileRouter.delete(
+  "/deleteProfile",
+  authMiddleware,
+  profileController.deleteProfile
+);
 
 module.exports = profileRouter;
